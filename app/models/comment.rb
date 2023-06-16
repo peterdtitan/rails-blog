c7b49b1c35b3937e3f0c2fc72ac5b59294849450
Peter Okorafor <peterdtitan@gmail.com>
2023-06-16 13:05:28 +0300

    Add tests and validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
   after_save :update_comment_counter
+  after_destroy :update_comment_counter
 
   private
 
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,11 @@ class Like < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
+  after_save :update_like_counter
+  after_destroy :update_like_counter
+
+  private
+
   def update_like_counter
     post.update(likes_counter: post.likes.count)
   end
