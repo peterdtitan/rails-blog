46b9587768cdf3de903197281967da782a7dfce9
Peter Okorafor <peterdtitan@gmail.com>
2023-06-20 18:14:20 +0300

    Final changes to posts and users controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
 class PostsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+
+    return unless @post.nil?
+
+    redirect_to user_posts_path(@user)
+  end
 end
