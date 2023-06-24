e8baf3a9f8b853aeb0cafc4a62ab6d51dbad1209
Peter Okorafor <peterdtitan@gmail.com>
2023-06-24 15:17:31 +0300

    Added form logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,23 @@ class PostsController < ApplicationController
 
     redirect_to user_posts_path(@user)
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+
+    if @post.save
+      redirect_to user_post_path(@user, @post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
