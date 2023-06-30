3f2215a4d8a99fc1c3b3bd52c99ce77d8b98b30b
Peter Okorafor <peterdtitan@gmail.com>
2023-06-30 21:13:32 +0300

    Update user view and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable, :confirmable
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
