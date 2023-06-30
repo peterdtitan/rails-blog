8a29e28b0b870c08076baf16c8a29086a90055ba
Peter Okorafor <peterdtitan@gmail.com>
2023-06-30 21:10:23 +0300

    Changes to controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,33 +1,31 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @posts = @user.posts.page(params[:page]).per(2)
+    @recent_comments = Comment.where(post_id: @posts)
+      .includes(:user)
+      .select('DISTINCT ON (post_id) *')
+      .order(:post_id, created_at: :desc)
+      .limit(5)
   end
-
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find_by(id: params[:id])
-
     return unless @post.nil?
-
     redirect_to user_posts_path(@user)
   end
-
   def create
     @user = User.find(params[:user_id])
     @post = @user.posts.build(post_params)
     @post.comments_counter = 0
     @post.likes_counter = 0
-
     if @post.save
       redirect_to user_post_path(@user, @post)
     else
       render :new
     end
   end
-
   private
-
   def post_params
     params.require(:post).permit(:title, :text)
   end
