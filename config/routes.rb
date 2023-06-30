32c49823cec2dc3e88e7f02c54a2b64067988090
Peter Okorafor <peterdtitan@gmail.com>
2023-06-30 21:14:24 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'users#index'
+
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show create new] do
       resources :comments, only: %i[create new]
