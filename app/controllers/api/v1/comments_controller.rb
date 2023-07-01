8221be2677db339213b14e52ed815370b87b3794
SabiMantock <sabimantock@gmail.com>
2023-07-01 11:33:59 +0000

    feat: create endpoint to list all comments and add a comment to a post

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::CommentsController < ApplicationController
+ 
+  skip_before_action :verify_authenticity_token
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+     render json: @comments
+    @comment.user = current_user
+
+    if @comment.save
+      render json: @comment, status: :created
+    else
+           render json: { error: 'Failed to create comment' }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
       end
     end
   end
+
+
+
 end
